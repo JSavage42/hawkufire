86367097e2e35aad01a839c4c84af627f6cf2ebf
JSavage42 <jasavage42@gmail.com>
2019-02-26 19:32:45 -0600

    Updates

---
+++ precache-manifest.202584bc4c29d81045f3c0dac8100767.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa0b0758c4ae5918bb48",
+    "url": "/hawkufire/static/css/main.58dc36e8.chunk.css"
+  },
+  {
+    "revision": "fa0b0758c4ae5918bb48",
+    "url": "/hawkufire/static/js/main.fa0b0758.chunk.js"
+  },
+  {
+    "revision": "714cd8d1f747deedb0c0",
+    "url": "/hawkufire/static/js/1.714cd8d1.chunk.js"
+  },
+  {
+    "revision": "eb073ce2797dadd17fc8",
+    "url": "/hawkufire/static/js/runtime~main.eb073ce2.js"
+  },
+  {
+    "revision": "c75e947786fa1d8c2b0ca02da2109ced",
+    "url": "/hawkufire/index.html"
+  }
+];
